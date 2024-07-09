7b19358071dfc2eab4e92014991b4f83dd23ee2d
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-09 19:11:39 +0200

    [UPD] Esercizio 1 con implemento al DOM completato

--- primo esercizio base/script/script.js
+++ primo esercizio base/script/script.js
@@ -1,8 +1,25 @@
-//Chiedi all'utente di inserire il suo nome utilizzando prompt()..
-let userName = prompt('come ti chiami?');
+//solo su console
 
-//Crea un messaggio di saluto personalizzato che includa il nome dell'utente.
-let message = 'Ciao' + " " + userName + " " + 'Benvenuto in questo primo esercizio!';
+// //Chiedi all'utente di inserire il suo nome utilizzando prompt()..
+// let userName = prompt('come ti chiami?');
 
-//Mostra il messaggio di saluto all'utente utilizzando alert().
-alert(message);
+// //Crea un messaggio di saluto personalizzato che includa il nome dell'utente.
+// let message = 'Ciao' + " " + userName + " " + 'Benvenuto in questo primo esercizio!';
+
+// //Mostra il messaggio di saluto all'utente utilizzando alert().
+// alert(message);
+
+
+//implementiamolo anche sul dom! 
+
+//xreo l'evento click sul button send e dentro aggiungo tutto ciò che deve succedere 
+document.getElementById("send").addEventListener("click", function () {
+
+    //recupero il valore inserito dall'utente da un input
+    let userName = document.getElementById("userName").value;
+
+    //Crea un messaggio di saluto personalizzato che includa il nome dell'utente.
+    let welcomeMessage = 'Ciao ' + userName + ' Benvenuto in questo primo esercizio!';
+
+    document.getElementById('welcome').textContent = welcomeMessage;
+});
