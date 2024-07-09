e9dbb6ae7c359b5e95b177718daf9b658c6a542a
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-09 18:41:41 +0200

    [UPD] Esercizio 1 terminato

---
+++ primo esercizio base/script/script.js
@@ -0,0 +1,8 @@
+//Chiedi all'utente di inserire il suo nome utilizzando prompt()..
+let userName = prompt('come ti chiami?');
+
+//Crea un messaggio di saluto personalizzato che includa il nome dell'utente.
+let message = 'Ciao' + " " + userName + " " + 'Benvenuto in questo primo esercizio!';
+
+//Mostra il messaggio di saluto all'utente utilizzando alert().
+alert(message);
